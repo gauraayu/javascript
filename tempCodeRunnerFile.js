630bb5f5cbb07e3e744b260c5a71fd8f5ff1daec
gauraayu <gauraayushi2004@gmail.com>
2025-05-14 19:27:50 +0530

    update

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,7 +1,2 @@
-let j="Hello World! World";
-let k=j.replace("World","hehehe");
-console.log(k);
-// -------------to globally replace all the words without using replace all
-console.log(j.replace(/World/g,"hehehe"));
-// -------------case insensitive replace all
-console.log(j.replace(/world/gi,"hehehe"));
+console.log(a) //undefined
+var a=10
