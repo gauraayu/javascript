61b0e111b450b2edb5727648214050d19f71fa27
gauraayu <gauraayushi2004@gmail.com>
2025-05-16 19:20:51 +0530

    update

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,2 +1,10 @@
-console.log(a) //undefined
-var a=10
+let arr5=[1,2,3];
+let arr6=[4,5,6];
+let mergedarr=[];
+for(let i=0;i<arr5.length;i++){
+    mergedarr.push(arr5[i]);
+}
+console.log(mergedarr);
+for(let i=0;i<arr6.length;i++){
+    mergedarr.push(arr6[i]);
+}
